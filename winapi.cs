bd9de364a3846fec4505726f3e9655530c714a06
erwan.vivien <erwan.vivien@epita.fr>
2020-12-28 09:34:42 +0100

    Functions: moved all functions to winapi class

--- winapi.cs
+++ winapi.cs
@@ -1,7 +1,66 @@
+using System;
+
 namespace ChatBot
 {
     class Winapi
     {
+        static public void act_upward()
+        {
+            Console.WriteLine("FCT: upward");
+        }
+        static public void act_a()
+        {
+            Console.WriteLine("FCT: a");
+        }
+        static public void act_b()
+        {
+            Console.WriteLine("FCT: b");
+        }
+        static public void act_x()
+        {
+            Console.WriteLine("FCT: x");
+        }
+        static public void act_y()
+        {
+            Console.WriteLine("FCT: y");
+        }
+        static public void act_l1()
+        {
+            Console.WriteLine("FCT: y");
+        }
+        static public void act_l2()
+        {
+            Console.WriteLine("FCT: y");
+        }
 
+        static public void act_r1()
+        {
+            Console.WriteLine("FCT: y");
+        }
+
+        static public void act_r2()
+        {
+            Console.WriteLine("FCT: y");
+        }
+
+        static public void act_nothing()
+        {
+            Console.WriteLine("FCT: nothing");
+        }
+
+        static public void act_downward()
+        {
+            Console.WriteLine("FCT: downward");
+        }
+
+        static public void act_left()
+        {
+            Console.WriteLine("FCT: left");
+        }
+
+        static public void act_right()
+        {
+            Console.WriteLine("FCT: right");
+        }
     }
 }
