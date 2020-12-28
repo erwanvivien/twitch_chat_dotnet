968a2ab490f25f6d757fc095355fbf50fe6d7a45
erwan.vivien <erwan.vivien@epita.fr>
2020-12-28 09:33:46 +0100

    Stream: Added time check and count of possibilities

--- stream.cs
+++ stream.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading;
 
@@ -16,29 +17,14 @@ namespace ChatBot
         protected string server = null;
         protected string bot_name = "EMU_DS";
 
-        public abstract void platform();
+        // Usefull for future platforms
+        public abstract List<string[]> read();
+        public abstract void start();
     }
 
 
     class Twitch : Bot
     {
-        static public void upward()
-        {
-            Console.WriteLine("upward");
-        }
-        static public void downward()
-        {
-            Console.WriteLine("downward");
-        }
-        static public void left()
-        {
-            Console.WriteLine("left");
-        }
-        static public void right()
-        {
-            Console.WriteLine("right");
-        }
-
         private IRC irc = null;
 
         private delegate void myDelegate();
@@ -46,12 +32,33 @@ namespace ChatBot
         private Dictionary<string, myDelegate> possibilities =
             new Dictionary<string, myDelegate>
             {
-                { "upward", new myDelegate(upward) },
-                { "downward", new myDelegate(downward) },
-                { "left", new myDelegate(left) },
-                { "right", new myDelegate(right) },
+                /// Movements
+                { "up", new myDelegate(Winapi.act_upward) },
+                { "down", new myDelegate(Winapi.act_downward) },
+                { "left", new myDelegate(Winapi.act_left) },
+                { "right", new myDelegate(Winapi.act_right) },
+
+                { "a", new myDelegate(Winapi.act_a) },
+                { "b", new myDelegate(Winapi.act_b) },
+                { "x", new myDelegate(Winapi.act_x) },
+                { "y", new myDelegate(Winapi.act_y) },
+
+                { "l1", new myDelegate(Winapi.act_l1) },
+                { "l2", new myDelegate(Winapi.act_l2) },
+
+                { "r1", new myDelegate(Winapi.act_r1) },
+                { "r2", new myDelegate(Winapi.act_r2) },
+
+                { "void", new myDelegate(Winapi.act_nothing) },
             };
 
+        // This should be exactly like above one with zeroes (set in `set`)
+        private Dictionary<string, int> possibilities_count = new Dictionary<string, int>();
+
+        public override List<string[]> read()
+        {
+            return irc.read();
+        }
 
         private void set(string channel, string password, string server, string port, string bot_name)
         {
@@ -63,23 +70,55 @@ namespace ChatBot
             this.port = port_nb;
             this.bot_name = bot_name;
 
+            // Inits IRC payload
             this.irc = new IRC(server, port_nb, bot_name, password, channel);
+
+            // Inits `possibilities_count` with same KEY
+            possibilities.Keys.ToList().ForEach(x => possibilities_count.Add(x, 0));
         }
 
-        public override void platform()
+        public override void start()
         {
+            double timer = 2;
+            double now = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
             while (true)
             {
-                List<string[]> tmp = irc.read();
+                // Checks if time is ()
+                double current = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                if (now / 1000 + timer <= current / 1000)
+                {
+                    double passed = current - now;
+                    Console.WriteLine($"ACT: {passed} miliseconds have passed");
+
+                    // Resets timer
+                    now = current;
+
+                    /// Will return the max value
+                    var keyOfMaxValue = possibilities_count.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+                    // Console.WriteLine(keyOfMaxValue);
+                    possibilities_count.Keys.ToList().ForEach(x => possibilities_count[x] = 0);
+
+                    // Executes max key function
+                    possibilities[keyOfMaxValue]();
+                }
+
+                // Reads chat (calls irc.read())
+                List<string[]> tmp = read();
+
+                // Does nothing if error / empty list
                 if (tmp == null || tmp.Count == 0)
                     continue;
 
+                // Updates counts
                 foreach (var e in tmp)
                 {
                     string name = e[0], content = e[1];
                     Console.WriteLine($"<- {name}: '{content}'");
-                    if (possibilities.ContainsKey(content))
-                        possibilities[content]();
+                    content = content.ToLower();
+
+                    if (possibilities_count.ContainsKey(content))
+                        possibilities_count[content]++;
                 }
             }
         }
